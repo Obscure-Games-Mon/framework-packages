9bda5307d513aa801db61e6e84560949128f8cf5
darketomaly@gmail.com <>
2024-01-21 05:10:06 +0800

    - Mathematics class is no longer part of Core class and Mathematics namespace extensions are declared on Mathematics class, avoiding to have multiple Core prompts by the ide (one from `Framework` and another for `Framework.Mathematics`). From now on, partial classes will be exclusively used within the same namespace to avoid this in a future - Added a test script outside of `com.erkmoz.framework.core` folder to test packages references

---
+++ Assets/Test/Test.cs
@@ -0,0 +1,16 @@
+using Framework;
+using Framework.Mathematics;
+using UnityEngine;
+
+namespace TestNamespace
+{
+    public class Test : MonoBehaviour
+    {
+        private void TestMethod()
+        {
+            float tmp = 0;
+            tmp.MapValue(0, 0, 0, 0);
+            this.Log(tmp.ToString("C"));
+        }
+    }
+}
