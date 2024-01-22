1ac2f52d263040749b8828498a3e320cf64e8bba
darketomaly@gmail.com <>
2024-01-22 16:55:26 +0800

    Wizard window saves color and type of logs to enable (from the framework) - code has yet to use editor preferences

--- Assets/com.erkmoz.framework.core/Scripts/Runtime/Core.cs
+++ Assets/com.erkmoz.framework.core/Scripts/Runtime/Core.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Framework
 {
@@ -43,5 +48,34 @@ namespace Framework
         #endregion
         
         #endregion
+
+        public static class Prefs
+        {
+            private static readonly Dictionary<string, string> Keys = new Dictionary<string, string>()
+            {
+                {Key.LogColor, "627bc4"},
+                {Key.ImportantLogColor, "ab953c"},
+                {Key.WarningLogColor, "b988d1"},
+                {Key.ErrorLogColor, "d9766f"}
+            };
+            
+            public class Key
+            {
+                public const string LogColor = "LogColor";
+                public const string ImportantLogColor = "ImportantLogColor";
+                public const string WarningLogColor = "WarningLogColor";
+                public const string ErrorLogColor = "ErrorLogColor";
+                
+                public const string Logs = "Logs";
+            }
+
+            public static Color GetColor(string key)
+            {
+                var colorString = EditorPrefs.GetString(key, Keys[key]);
+                ColorUtility.TryParseHtmlString($"#{colorString}", out Color parsedColor);
+                
+                return parsedColor;
+            }
+        }
     }
 }
