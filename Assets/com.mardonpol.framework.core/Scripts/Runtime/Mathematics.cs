32af23b451f2b3c0e0756f2b96933149ae6fc828
create@darketomaly.hk <>
2024-05-10 19:44:53 +0800

    Separate extension method that can work with any generic, not just objects

--- Assets/com.mardonpol.framework.core/Scripts/Runtime/Mathematics.cs
+++ Assets/com.mardonpol.framework.core/Scripts/Runtime/Mathematics.cs
@@ -35,5 +35,28 @@ namespace Framework.Mathematics
             
             return defaultValue;
         }
+        
+        /// <summary>
+        /// Safely retrieves an element from an array of generics.
+        /// </summary>
+        /// <typeparam name="T">The type of the desired element.</typeparam>
+        /// <param name="objectArray">The array of generics.</param>
+        /// <param name="index">The index of the desired element.</param>
+        /// <param name="defaultValue">The default value to return if the element is not found.</param>
+        /// <returns>The element at the specified index or the default value.</returns>
+        public static T SafeGetAt<T>(this T[] objectArray, int index, T defaultValue = default)
+        {
+            if (objectArray != null && objectArray.Length > index && index >= 0)
+            {
+                object obj = objectArray[index];
+
+                if (obj is T obj1)
+                {
+                    return obj1;
+                }
+            }
+            
+            return defaultValue;
+        }
     }
 }
