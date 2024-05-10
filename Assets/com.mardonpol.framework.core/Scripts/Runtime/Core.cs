4b80725d5d44f86c69ba9292e3e6a030f3ce2f5f
create@darketomaly.hk <>
2024-05-11 07:07:49 +0800

    Moved back extension methods, updated package.json, added empty package with dependency

--- Assets/com.mardonpol.framework.core/Scripts/Runtime/Core.cs
+++ Assets/com.mardonpol.framework.core/Scripts/Runtime/Core.cs
@@ -107,6 +107,52 @@ namespace Framework
         
         #endregion
         
+        /// <summary>
+        /// Safely retrieves an element from an array of objects.
+        /// </summary>
+        /// <typeparam name="T">The type of the desired element.</typeparam>
+        /// <param name="objectArray">The array of objects.</param>
+        /// <param name="index">The index of the desired element.</param>
+        /// <param name="defaultValue">The default value to return if the element is not found.</param>
+        /// <returns>The element at the specified index or the default value.</returns>
+        public static T SafeGetAt<T>(this object[] objectArray, int index, T defaultValue = default)
+        {
+            if (objectArray != null && objectArray.Length > index && index >= 0)
+            {
+                object obj = objectArray[index];
+
+                if (obj is T obj1)
+                {
+                    return obj1;
+                }
+            }
+            
+            return defaultValue;
+        }
+        
+        /// <summary>
+        /// Safely retrieves an element from an array of generics.
+        /// </summary>
+        /// <typeparam name="T">The type of the desired element.</typeparam>
+        /// <param name="objectArray">The array of generics.</param>
+        /// <param name="index">The index of the desired element.</param>
+        /// <param name="defaultValue">The default value to return if the element is not found.</param>
+        /// <returns>The element at the specified index or the default value.</returns>
+        public static T SafeGetAt<T>(this T[] objectArray, int index, T defaultValue = default)
+        {
+            if (objectArray != null && objectArray.Length > index && index >= 0)
+            {
+                object obj = objectArray[index];
+
+                if (obj is T obj1)
+                {
+                    return obj1;
+                }
+            }
+            
+            return defaultValue;
+        }
+        
         public static class Prefs
         {
             private static readonly Dictionary<string, string> Keys = new Dictionary<string, string>()
--- Assets/com.mardonpol.framework.core/Scripts/Runtime/Mathematics.cs
+++ Assets/com.mardonpol.framework.core/Scripts/Runtime/Mathematics.cs
@@ -12,51 +12,5 @@ namespace Framework.Mathematics
             float value = b0 + (b1 - b0) * ((x - a0) / (a1 - a0));
             return Mathf.Clamp(value, Mathf.Min(b0, b1), Mathf.Max(b0, b1));
         }
-        
-        /// <summary>
-        /// Safely retrieves an element from an array of objects.
-        /// </summary>
-        /// <typeparam name="T">The type of the desired element.</typeparam>
-        /// <param name="objectArray">The array of objects.</param>
-        /// <param name="index">The index of the desired element.</param>
-        /// <param name="defaultValue">The default value to return if the element is not found.</param>
-        /// <returns>The element at the specified index or the default value.</returns>
-        public static T SafeGetAt<T>(this object[] objectArray, int index, T defaultValue = default)
-        {
-            if (objectArray != null && objectArray.Length > index && index >= 0)
-            {
-                object obj = objectArray[index];
-
-                if (obj is T obj1)
-                {
-                    return obj1;
-                }
-            }
-            
-            return defaultValue;
-        }
-        
-        /// <summary>
-        /// Safely retrieves an element from an array of generics.
-        /// </summary>
-        /// <typeparam name="T">The type of the desired element.</typeparam>
-        /// <param name="objectArray">The array of generics.</param>
-        /// <param name="index">The index of the desired element.</param>
-        /// <param name="defaultValue">The default value to return if the element is not found.</param>
-        /// <returns>The element at the specified index or the default value.</returns>
-        public static T SafeGetAt<T>(this T[] objectArray, int index, T defaultValue = default)
-        {
-            if (objectArray != null && objectArray.Length > index && index >= 0)
-            {
-                object obj = objectArray[index];
-
-                if (obj is T obj1)
-                {
-                    return obj1;
-                }
-            }
-            
-            return defaultValue;
-        }
     }
 }
